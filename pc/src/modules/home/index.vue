51895bb337bfb2940e6c6ba061a609507f2f1ff4
JuJenJie <zhuzhenjie@Z-MacBook.local>
2019-01-09 11:00:59 +0800

    update:home

--- pc/src/modules/home/index.vue
+++ pc/src/modules/home/index.vue
@@ -1,18 +1,38 @@
 <style lang="scss" scoped>
 .home {
-    text-align: center;
-    font-size: 22px;
-    line-height: 60px;
+    // text-align: center;
+    width: 300px;
+    margin: 0 auto;
+
+    .title {
+        color: #666;
+    }
+
+    .name {
+        font-size: 22px;
+    }
+
+    .item {
+        padding-bottom: 30px;
+    }
+
 }
 </style>
 <template>
     <div class="home">
         <h1>demo</h1>
-        <div>
-            <router-link :to="{name:'jStrongTable'}">jStrongTable</router-link>
+        <h4 class="title">原生js(不依赖任何框架)</h4>
+        <div class="item">
+            <div class="title">多功能表格</div>
+            <div class="name">
+                <router-link :to="{name:'jStrongTable'}">jStrongTable</router-link>
+            </div>
         </div>
-        <div>
-            <router-link :to="{name:'jTimePicker'}">jTimePicker</router-link>
+        <div class="item">
+            <div class="title">日期选择器</div>
+            <div class="name">
+                <router-link :to="{name:'jTimePicker'}">jTimePicker</router-link>
+            </div>
         </div>
     </div>
 </template>
