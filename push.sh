22a70f786e24e0891d6e22ae59f47a7876a883ba
jujenjie <523540709@qq.com>
2021-01-13 19:46:25 +0800

    自动提交 2021-01-13 19:46:24

--- push.sh
+++ push.sh
@@ -1,13 +1,14 @@
 ###
 # @Author: your name
 # @Date: 2021-01-13 16:58:29
- # @LastEditTime: 2021-01-13 17:49:56
+ # @LastEditTime: 2021-01-13 19:46:24
  # @LastEditors: Please set LastEditors
 # @Description: In User Settings Edit
 # @FilePath: /desktop-tutorial/push.sh
 ###
 dates=`date "+%Y-%m-%d"` #日期
 times=`date "+%H:%M:%S"` #时间
+blaches=`master`
 
 isGit=$(ls -a | grep -x .git | wc -l) #判断是否有仓库
 if [ $isGit -gt 0 ]; then
@@ -15,7 +16,7 @@ if [ $isGit -gt 0 ]; then
 	if [ $isAdd -gt 0 ]; then
 		git add .
 		git commit -m "自动提交 $dates $times"
-		git push origin main
+		git push origin $blaches
 		echo "+提交完成"
 	else
 		echo "-没有更改"
