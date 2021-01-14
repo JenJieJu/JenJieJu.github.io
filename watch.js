0b0a72b45c5183f7ea02a5e75de330fb93758655
jujenjie <523540709@qq.com>
2021-01-14 14:43:03 +0800

    自动提交 2021-01-14 14:43:03

--- watch.js
+++ watch.js
@@ -1,16 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2021-01-13 17:32:53
- * @LastEditTime: 2021-01-14 13:00:49
+ * @LastEditTime: 2021-01-14 14:42:55
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /desktop-tutorial/watch.js
  */
-const shell = require('shelljs')
+const shell = require("shelljs");
 var watch = require("node-watch");
 
-
-shell.exec('./push.sh')
+shell.exec("./push.sh", { async: true });
 
 watch(
   "./",
@@ -20,17 +19,16 @@ watch(
       // skip node_modules
       if (/\/node_modules/.test(f)) return skip;
       // skip .git folder
-			if (/\.git/.test(f)) return skip;
-			if (/\.history/.test(f)) return skip;
-			if (/\book/.test(f)) return skip;
+      if (/\.git/.test(f)) return skip;
+      if (/\.history/.test(f)) return skip;
+      if (/book/.test(f)) return skip;
       // only watch for js files
-      return true
+      return true;
     },
   },
   function (evt, name) {
-		console.log("%s changed.", name);
-		shell.exec('./build.sh')
-		shell.exec('./push.sh')
-		
+    console.log("%s changed.", name);
+    shell.exec("./build.sh", { async: true });
+    shell.exec("./push.sh", { async: true });
   }
 );
